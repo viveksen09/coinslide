3740068aaed8000261cc1ab53d6ec73f75b9da6b
Vivek Sen John Senin <viveksen09@gmail.com>
2020-05-02 21:33:27 -0400

    Refactored. Moved logo.png into public dir

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo = () => {
     return (
         <div>
-            <img src={require('../images/logo.png')} alt='company logo' style={{width: '200px' }}/>
+            <img src="/logo.png" alt='company logo' style={{width: '200px' }}/>
         </div>
     );
 };
