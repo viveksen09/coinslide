5ac427d1571353b9f6911042a712c3e0596b2115
Vivek Sen John Senin <viveksen09@gmail.com>
2020-06-16 14:08:42 -0400

    Added cors header

--- src/api/coingecko.js
+++ src/api/coingecko.js
@@ -1,5 +1,8 @@
 import Axios from 'axios';
 
 export default Axios.create({
-    baseURL: 'https://api.coingecko.com/api/v3'
+    baseURL: 'https://api.coingecko.com/api/v3',
+    headers: {
+        'Access-Control-Allow-Origin': '*'
+    }
 });
